50542bb2473b1f686664e626619fc31d0fdd6ad9
Dominic Huber <dominic.nadim@bluewin.ch>
2020-12-23 22:11:27 +0100

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Dominic Huber on 23.12.20.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
